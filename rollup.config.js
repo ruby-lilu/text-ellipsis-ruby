e6f3ad5c087b1f895f7df0d13092e92722b678dc
lilu <1363546250@qq.com>
2022-10-10 18:06:25 +0800

    build:v0.0.1

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
-import resolve from "rollup-plugin-node-resolve";
-import commonjs from "rollup-plugin-commonjs";
-import babel from "rollup-plugin-babel";
+import resolve from "rollup-plugin-node-resolve"; // 帮助寻找node_modules里的包
+import commonjs from "rollup-plugin-commonjs"; // 将非ES6语法的包转为ES6可用
+import babel from "rollup-plugin-babel"; // rollup 的 babel 插件，ES6转ES5
 import { terser } from "rollup-plugin-terser";
 const isDev = process.env.NODE_ENV !== "production";
 
@@ -13,8 +13,8 @@ export default [
       format: "umd",
     },
     plugins: [
-      resolve(), // 这样 Rollup 能找到 `ms`
-      commonjs(), // 这样 Rollup 能转换 `ms` 为一个ES模块
+      resolve(),
+      commonjs(),
       babel({
         exclude: "node_modules/**", // 防止打包node_modules下的文件
         runtimeHelpers: true, // 使plugin-transform-runtime生效
